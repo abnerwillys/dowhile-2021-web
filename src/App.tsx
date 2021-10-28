ceaf52e9b70a219b53911b27fd1619c94f941ad0
Abner Willys <abwillys001@gmail.com>
2021-10-28 01:45:32 -0300

    structure and styling of SendMessageForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+
+import { LoginBox } from "./components/LoginBox";
 import { MessageList } from "./components/MessageList";
+import { SendMessageForm } from "./components/SendMessageForm";
+
+import { useAuthContext } from "./hooks/useAuthContext";
 
 import styles from './App.module.scss'
-import { LoginBox } from "./components/LoginBox";
 
 export function App() {
+  const { user } = useAuthContext()
+
   return (
     <main className={styles.contentWrapper} >
       <MessageList />
-      <LoginBox />
+      { !!user ? <SendMessageForm />  : <LoginBox />}
     </main>
   )
 }
